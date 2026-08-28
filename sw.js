a9300eb955f5666de95f43cfd07c0b442aae9270
CarlTherock <chassextremehunting@gmail.com>
2026-08-27 22:52:16 -0400

    Bump cache to v3 to force update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cb-alerte-v2';
+const CACHE = 'cb-alerte-v3';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
